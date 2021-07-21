4da1d4e867e24e063766be17d15281d293d02135
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-07-21 12:44:15 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet'
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,33 +13,31 @@ function App() {
   return (
     <Router>
       <>
-        <Helmet>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/one">One</Link>
-            </li>
-            <li>
-              <Link to="/two">Two</Link>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/one">One</Link>
+          </li>
+          <li>
+            <Link to="/two">Two</Link>
+          </li>
+        </ul>
 
-          <hr />
+        <hr />
 
-          <Switch>
-          <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/one">
-              <One />
-            </Route>
-            <Route path="/two">
-              <Two />
-            </Route>
-          </Switch>
-        </Helmet>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/one">
+            <One />
+          </Route>
+          <Route path="/two">
+            <Two />
+          </Route>
+        </Switch>
       </>
     </Router>
   );
