ec5f47d83220436bdf47e0fd4469a6de12a5eb18
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-07-21 18:19:33 +0700

    Sukak kau la ipin

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,20 @@ function Home() {
     return (
         <>
             <Helmet>
-                <title>Home</title>
-                <meta name="title" content="One GG Asli" />
-                <meta name="description" content="One Bray Description" />
+                <title>your keyword rich title of the website and/or webpage</title>
+                <meta name="description" content="description of your website/webpage, make sure you use keywords!" />
 
-                <meta property="og:title" content="Home Bray" />
-                <meta property="og:description" content="This is Home Description" />
+                <meta property="og:title" content="short title of your website/webpage" />
+                <meta property="og:url" content="https://master.dgmlkjvqxsdr9.amplifyapp.com" />
+
+                <meta property="og:description" content="description of your website/webpage" />
+                <meta property="og:image" content="//cdn.example.com/uploads/images/webpage_300x200.png" />
+                <meta property="og:type" content="article" />
+
+                <meta property="og:locale" content="en_GB" />
+                <meta property="og:locale:alternate" content="fr_FR" />
+                <meta property="og:locale:alternate" content="es_ES" />
+
             </Helmet>
             Home
         </>
