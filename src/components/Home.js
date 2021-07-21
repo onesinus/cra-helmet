f57963c1ad6ccf77e0a509e87f9b87960e4586b4
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-07-21 18:32:57 +0700

    gue salah coding, codingannya yang minta maap, yasalam

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,7 @@ function Home() {
 
             </Helmet>
             Home
+            <img src="https://d2jsr68bvg2sg0.cloudfront.net/otobo_executive.png" alt="Coba Gambar" />
         </>
     )
 }
