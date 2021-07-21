17cf08b4a8d041455d35761aa23a143b4f4cae14
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-07-21 17:58:24 +0700

    Yasalam belum jalan juga

--- src/components/Two.js
+++ src/components/Two.js
@@ -4,10 +4,11 @@ function Two() {
     return (
         <>
             <Helmet>
+                <meta name="description" content="One Bray Description" data-rh="true" />
                 <meta property="og:site_name" content="San Roque 2014 Pollos" data-rh="true" />
                 <meta property="og:title" content="Two Bray" data-rh="true" />
                 <meta property="og:description" content="This is Two Description" data-rh="true" />
-                <meta property="og:image" itemprop="image" content="http://pollosweb.wesped.es/programa_pollos/play.png" data-rh="true" />
+                <meta property="og:image" itemprop="image" content="https://www.w3schools.com/html/pic_trulli.jpg" data-rh="true" />
                 <meta property="og:type" content="website" data-rh="true" />
                 <meta property="og:updated_time" content="1440432930" data-rh="true" />
                 <title>Two</title>
